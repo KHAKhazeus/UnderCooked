cd6aff3200e4e387105fa2c14a04ada3fa56b972
Circle <1176092962@qq.com>
2019-10-31 00:24:04 +0800

    log4j added

--- src/main/java/visitor/CarMaintainer.java
+++ src/main/java/visitor/CarMaintainer.java
@@ -1,17 +1,24 @@
 package visitor;
 
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class CarMaintainer implements Visitor {
+    public static final Logger logger = LogManager.getLogger();
+
     public void visit(Wheel wheel) {
-        System.out.println("Maintaining " + wheel.getName()
+        logger.info("Maintaining " + wheel.getName()
                 + " wheel");
+        logger.info(this);
     }
     public void visit(Engine engine) {
-        System.out.println("Maintaining engine");
+        logger.info("Maintaining engine");
     }
     public void visit(Body body) {
-        System.out.println("Maintaining body");
+        logger.info("Maintaining body");
     }
     public void visit(Car car) {
-        System.out.println("Maintaining car");
+        logger.info("Maintaining car");
     }
 }
