9633ef19fc33beac1d6a246faf0d66c187eddd65
PtNan <imasxu@gmail.com>
2019-11-02 23:19:58 +0800

    bug fix-- memento, publishSubscriber, futurePromise, immutable

---
+++ src/main/java/futurePromise/Cooks.java
@@ -0,0 +1,17 @@
+package futurePromise;
+// Cook 接口的实现
+
+public class Cooks implements Cook {
+    private String name;
+
+    Cooks(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void UpdateMsg(String msg) {
+        if (!"".equals(msg)) {
+            System.out.println(name + ": get " + msg);
+        }
+    }
+}
---
+++ src/main/java/immutable/Cook.java
@@ -0,0 +1,6 @@
+package immutable;
+// Cooks 的接口
+
+public interface Cook {
+    public void UpdateMsg(String msg);
+}
