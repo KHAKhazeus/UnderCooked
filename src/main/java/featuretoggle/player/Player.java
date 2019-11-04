e4274c8bd9e17ac4c8a3df67583099199d4ea93a
Circle <1176092962@qq.com>
2019-11-04 22:54:36 +0800

    Feature Toggle Pattern

---
+++ src/main/java/featuretoggle/player/Player.java
@@ -0,0 +1,14 @@
+package featuretoggle.player;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+/**
+ * 游戏玩家类
+ */
+@AllArgsConstructor
+@Getter
+public class Player {
+    private String name;
+}
--- src/test/java/Adapter/AdapterTest.java
+++ src/test/java/Adapter/AdapterTest.java
@@ -1,6 +1,6 @@
 package Adapter;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
